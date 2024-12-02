9147e68820a760f2374c1929e0d1ce764c2d893c
martin <guerreiromartin@gmail.com>
2024-12-02 11:55:10 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), robotsTxt()],
   site: "http://localhost:4321/",
+  output: "server",
+  adapter: vercel(),
 });
